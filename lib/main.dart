342735fe45aa57a99e64c5c77ec15089dee80898
URIEL_ISAI_AVILA_ROMAN <a.20308051280483@cbtis128.edu.mx>
2023-04-20 17:26:27 +0000

    v2

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,126 @@
 import 'package:flutter/material.dart';
+// Uncomment lines 7 and 10 to view the visual layout at runtime.
+// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void main() {
+  // debugPaintSizeEnabled = true;
+  runApp(MyApp());
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'No Country For Old Men',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Dirigida por: Ethan Coen, Joel Coen',
+                  style: TextStyle(
+                    color: Color(0xff141414),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          Text(
+            '93',
+          ),
+          Icon(
+            Icons.percent,
+            color: Colors.red[500],
+          ),
+        ],
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(Colors.purple, Icons.movie, 'Ver'),
+          _buildButtonColumn(Colors.purple, Icons.info, 'Sinopsis'),
+          _buildButtonColumn(Colors.purple, Icons.library_add, 'Calificar'),
+        ],
+      ),
+    );
+
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Lo que resalta en toda la película es la maldad de Chigurh, sí.'
+        'Pero aún más impactante es lo que provoca en el espectador.'
+        'Chigurh nunca se toca el corazón ante sus víctimas; ni aún cuando'
+        'para asesinarlo, tapa la cara de un narcotraficante mientras éste le suplica “no me'
+        'mates por favor”. La maldad que profesa Chigurgh tiene que ver con la diversión, con'
+        'comparar el asesinato con algo tan fácil como respirar  o lanzar una moneda al aire.',
+        softWrap: true,
+      ),
+    );
+
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(appBarTheme: const AppBarTheme(color: Colors.blueGrey)),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title:
+              const Text('Blockbuster', style: TextStyle(color: Colors.yellow)),
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'images/No_country.png',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
         ),
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
